fe54d223249abef67e2165ad7969aac284ff580e
Anya Keller <anyaisanya@gmail.com>
2016-05-17 10:14:04 -0400

    iqwjefl;kajsdf;laj

--- Treeify.java
+++ Treeify.java
@@ -3,7 +3,12 @@ public class Treeify{
 
 	//pre: 
 	//post: prints 
-	public static void tree(
+	public static void tree(BST<Integer> b){
+		String ans = "";
+		ans += b._root.getValue() + "";
+		
+		
+	}
 
 	public static void main(String[] args){
 		
