20e782e9058381208e33d46da693af4291f465f6
Anya Keller <anyaisanya@gmail.com>
2016-06-06 10:16:04 -0400

    Asdf

--- DSTAlgo.java
+++ DSTAlgo.java
@@ -12,71 +12,71 @@ public class DSTAlgo<E>{
 		}
 		return x;
 	}	
-	
-	
-    public TreeNode<E> rotateRight(TreeNode<E> x){
-	if (x.getLeft() != null){
-	    TreeNode<E> left = x.getLeft();
-	    x.setLeft(left.getLeft());
-	    left.setLeft(left.getRight());
-	    left.setRight(x.getRight());
-	    x.setRight(left);
+
+
+	public TreeNode<E> rotateRight(TreeNode<E> x){
+		if (x.getLeft() != null){
+			TreeNode<E> left = x.getLeft();
+			x.setLeft(left.getLeft());
+			left.setLeft(left.getRight());
+			left.setRight(x.getRight());
+			x.setRight(left);
+		}
+		return x;
 	}
-	return x;
-    }
-  
-    public TreeNode<E> makeRightVine(TreeNode<E> rt){
-	while(rt.getLeft() != null){
-	    rt = rotateRight(rt);
+
+	public TreeNode<E> makeRightVine(TreeNode<E> rt){
+		while(rt.getLeft() != null){
+			rt = rotateRight(rt);
+		}
+		if (rt.getRight() != null)
+			rt.setRight(makeRightVine(rt.getRight()));
+		return rt;
 	}
-	if (rt.getRight() != null)
-	    rt.setRight(makeRightVine(rt.getRight()));
-	return rt;
-    }
-  
-    public int nodeCount(TreeNode<E> rt){
-	if (rt == null)
-	    return 0;
-	int x = 1;
-	while (rt.getRight() != null){
-	    rt = rt.getRight();
-	    x++;
+
+	public int nodeCount(TreeNode<E> rt){
+		if (rt == null)
+			return 0;
+		int x = 1;
+		while (rt.getRight() != null){
+			rt = rt.getRight();
+			x++;
+		}
+		return x;
 	}
-	return x;
-    }
-  
-    public TreeNode<E> balanceNodes(TreeNode<E> rt, int nc){ //nc = node count
-	int t = (int)(Math.log((double)nc)/(Math.log((double)2)));
-	TreeNode<E> newRt = rt;
-	for (int i = 0; i < t; i++){
-	    newRt = rotateLeft(newRt);
-	    rt = newRt.getRight();
-	    for (int j = 0; j< nc / 2 - 1; j++){
-		rt = rotateLeft(rt);
-		rt = rt.getRight();
-	    }
-	    nc >>= 1;
+
+	public TreeNode<E> balanceNodes(TreeNode<E> rt, int nc){ //nc = node count
+		int t = (int)(Math.log((double)nc)/(Math.log((double)2)));
+		TreeNode<E> newRt = rt;
+		for (int i = 0; i < t; i++){
+			newRt = rotateLeft(newRt);
+			rt = newRt.getRight();
+			for (int j = 0; j< nc / 2 - 1; j++){
+				rt = rotateLeft(rt);
+				rt = rt.getRight();
+			}
+			nc >>= 1;
+		}
+		return newRt;
 	}
-	return newRt;
-    }
-    
-    public static void main(String [] args){
-    	 BST<Integer> tree = new BST<Integer>();
-    	tree.insert(5);
-    	tree.insert(4);
-	 tree.insert(3);
-	    tree.insert(2);
-	    tree.insert(1);
-	    tree.insert(6);
-	    tree.preorder();
-	    System.out.println(tree);
-	int nc = NodeCount(tree.rt());
-	balanceNodes(tree.rt(), nc);
-	    System.out.println(tree);
-    	
-    	
-    	
-    }
-    }
+
+	public static void main(String [] args){
+		BST tree = new BST();
+		tree.insert(5);
+		tree.insert(4);
+		tree.insert(3);
+		tree.insert(2);
+		tree.insert(1);
+		tree.insert(6);
+		tree.preorder();
+		System.out.println(tree);
+		int nc = nodeCount(tree.rt());
+		balanceNodes(tree.rt(), nc);
+		System.out.println(tree);
+
+
+
+
+	}
 
 }
